b930ab568b097fd743af3a788155812b31e1c8b2
bellavista <bellavista@phoenix-marie>
2024-12-12 17:53:51 +0100

    Update graf5.png, wooden_lady.jpg, demo.m, and 3 more files

--- matlab code/demo.m
+++ matlab code/demo.m
@@ -1,27 +1,35 @@
-function hz_demo
+function demo
 % HarrisZ and HarrisZ+ demo
 
 % tested on Ubuntu 20.04
 
 % you must have vlfeat installed for visualization, path must be set here 
-vlfeat_path='/home/user/MATLAB Add-Ons/vlfeat-0.9.21'; 
-run([vlfeat_path '/toolbox/vl_setup']);
+vlfeat_path=[];
+if isempty(vlfeat_path)
+    disp('vlfeat path not set in demo.m, no ellipses in the output visualization')
+else
+    run([vlfeat_path filesep 'toolbox' filesep 'vl_setup']);
+end
 
 %%% original HarrisZ
 % read image
-im=imread('graf5.png');
+im=imread(['..' filesep 'images' filesep 'graf5.png']);
 kpt_im_old=hz(im);
+
 % show keypoints
 figure;
 imshow(im);
 hold on;
-vl_plotframe(kpt_im_old);
-plot(kpt_im_old(1,:),kpt_im_old(2,:),'+r');
+if ~isempty(vlfeat_path)
+    vl_plotframe(kpt_im_old);
+end
+title('HarrisZ')
+plot(kpt_im_old(1,:),kpt_im_old(2,:),'+g');
 
 pause(0.5);
 %%% HarrisZ+
 % read image
-im=imread('graf5.png');
+im=imread(['..' filesep 'images' filesep 'graf5.png']);
 % max_kpt is used for the spatial uniform ranking
 max_kpts=8000;
 % or
@@ -34,14 +42,17 @@ kpt_im=kpt_im(:,1:min(size(kpt_im,2),max_kpts));
 figure;
 imshow(im);
 hold on;
-vl_plotframe(kpt_im);
-plot(kpt_im(1,:),kpt_im(2,:),'+r');
+if ~isempty(vlfeat_path)
+    vl_plotframe(kpt_im);
+end
+title('HarrisZ+')
+plot(kpt_im(1,:),kpt_im(2,:),'+g');
 
 pause(0.5);
 %%% HarrisZ+ for 
 % another example with higher resolution image and more keypoints
 % read image
-im_big=imread('wooden_lady.jpg');
+im_big=imread(['..' filesep 'images' filesep 'wooden_lady.jpg']);
 max_kpts=8000;
 % or
 % max_kpts=2048;
@@ -54,5 +65,8 @@ kpt_im_big=kpt_im_big(:,1:min(size(kpt_im_big,2),max_kpts));
 figure;
 imshow(im_big);
 hold on;
-vl_plotframe(kpt_im_big);
-plot(kpt_im_big(1,:),kpt_im_big(2,:),'+r');
+if ~isempty(vlfeat_path)
+    vl_plotframe(kpt_im_big);
+end
+title('HarrisZ+')
+plot(kpt_im_big(1,:),kpt_im_big(2,:),'+g');
